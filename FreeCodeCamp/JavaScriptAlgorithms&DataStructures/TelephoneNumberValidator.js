58dac05d7389369620b853bd3d06c53571302571
jlucian3 <jlucian3@binghamton.com>
2021-02-08 12:11:16 -0500

    reorg

--- FreeCodeCamp/JavaScriptAlgorithms&DataStructures/TelephoneNumberValidator.js
+++
@@ -1,5 +0,0 @@
-function telephoneCheck(str) {
-  return (/^(1\s?)?(\(\d{3}\)|\d{3})[\s-]?\d{3}[\s-]?\d{4}$/mg).test(str);
-}
-
-telephoneCheck("555-555-5555");
--- js/dynamicHeight.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-    var div = $('div');
-    var width = div.width();
-    
-    div.css('height', width);
-});
